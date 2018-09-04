fd104be842d1bd3ad658fc96c0f16d19d82bc41e
veturno <veturno@veturno>
2018-09-04 17:46:58 -0300

    a

---
+++ conceito/src/main/java/br/com/mvcs/model/Cliente.java
@@ -0,0 +1,51 @@
+package br.com.mvcs.model;
+
+public class Cliente {
+
+	private int id;
+	private String razaoSocial;
+	private String nomeFantasia;
+	private String cpfCnpj;
+	private String contato;
+	private String status;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+	public String getCpfCnpj() {
+		return cpfCnpj;
+	}
+	public void setCpfCnpj(String cpfCnpj) {
+		this.cpfCnpj = cpfCnpj;
+	}
+	public String getContato() {
+		return contato;
+	}
+	public void setContato(String contato) {
+		this.contato = contato;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	
+	
+}
---
+++ conceito/src/main/java/br/com/mvcs/persistance/ClienteDAO.java
@@ -0,0 +1,9 @@
+package br.com.mvcs.persistance;
+
+public class ClienteDAO {
+	
+	public void createCliente() {
+		
+	}
+
+}
