bd88a4ae8bc5313c10e33306ce64be7dea955e43
veturno <veturno@veturno>
2018-09-04 17:54:27 -0300

    Editando dao

--- conceito/src/main/java/br/com/mvcs/persistance/ClienteDAO.java
+++ conceito/src/main/java/br/com/mvcs/persistance/ClienteDAO.java
@@ -1,9 +1,21 @@
 package br.com.mvcs.persistance;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.mvcs.model.Cliente;
+
 public class ClienteDAO {
 	
 	public void createCliente() {
 		
+	}
+	
+	public List readCliente(Cliente cliente) {
+		List<ArrayList> clientes = new ArrayList();
+		
+		
+		return clientes;
 	}
 
 }
