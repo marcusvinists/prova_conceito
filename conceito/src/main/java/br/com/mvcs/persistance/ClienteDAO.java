d4a37d910e0c2e3d8750c77b7cd773c65932662f
vini <vini@DESKTOP-NM6T1PH>
2018-09-04 21:05:25 -0300

    dao

--- conceito/src/main/java/br/com/mvcs/persistance/ClienteDAO.java
+++ conceito/src/main/java/br/com/mvcs/persistance/ClienteDAO.java
@@ -3,18 +3,57 @@ package br.com.mvcs.persistance;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+
 import br.com.mvcs.model.Cliente;
 
 public class ClienteDAO {
 	
-	public void createCliente() {
-		
+	public static void createCliente(Cliente cliente) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			session.beginTransaction();
+			session.save(cliente);
+	        session.getTransaction().commit();
+		}catch (Exception e) {
+			e.printStackTrace();
+			 session.getTransaction().rollback();
+		}finally {
+			session.close();
+		}
 	}
 	
-	public List readCliente(Cliente cliente) {
-		List<ArrayList> clientes = new ArrayList();
-		
-		
+	
+	public static List<Cliente> readCliente(Cliente cliente) {
+		List<Cliente> clientes = new ArrayList<Cliente>();
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			Query<Cliente> query = null;
+			if (cliente.getId() > 0) {
+				query = session.createQuery("from cliente  where id = :id ");
+				query.setParameter("id",cliente.getId());
+				
+			}else if(cliente.getRazaoSocial() != null && !cliente.getRazaoSocial().isEmpty()) {
+				query = session.createQuery("from cliente  where razao_social = :rz ");
+				query.setParameter("rz",cliente.getRazaoSocial());
+				
+			}else if(cliente.getCpfCnpj() != null && !cliente.getCpfCnpj().isEmpty()) {				
+				query = session.createQuery("from cliente  where cnpj_cpf = :cnp ");
+				query.setParameter("cnp",cliente.getCpfCnpj());
+			}
+			
+			if(query != null) {
+				List<Cliente> list = query.list();
+				clientes = (List<Cliente>)list;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			session.close();
+		}
+
 		return clientes;
 	}
 
--- conceito/src/test/java/br/com/mvcs/teste/Tester.java
+++ conceito/src/test/java/br/com/mvcs/teste/Tester.java
@@ -3,26 +3,24 @@ package br.com.mvcs.teste;
 import org.hibernate.Session;
 
 import br.com.mvcs.model.Cliente;
+import br.com.mvcs.persistance.ClienteDAO;
 import br.com.mvcs.persistance.HibernateUtil;
 
 public class Tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		 
-        session.beginTransaction();
+	
         Cliente cliente = new Cliente();
  
         cliente.setRazaoSocial("Eireli etc abc");
-        cliente.setRazaoSocial("Venda do Zé");
+        cliente.setRazaoSocial("Venda do Ze");
         cliente.setCpfCnpj("364.350.128-57");
         cliente.setContato("(11) 971312661");
         cliente.setStatus("Ativo");
   
- 
-        session.save(cliente);
-        session.getTransaction().commit();
+        ClienteDAO.createCliente(cliente);
+     
  
 	}
 
